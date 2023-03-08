ce0b34faea4b51b6d26fe6ed7c99b3e1e7eaa4f6
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-03-08 15:29:45 +0900

    feature: 🔨 add svg type

--- src/components/custom.d.ts
+++ src/components/custom.d.ts
@@ -12,3 +12,11 @@ declare module 'styled-components' {
     breakpoints: (typeof theme)['breakpoints'];
   }
 }
+
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
