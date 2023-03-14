8422f24e114762929cfa05d2840aacab3a5f8142
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-03-14 10:51:29 +0900

    fix: 🔥 remove useless code
    remove unnecessary story code

--- src/components/molecules/buttons/UpDownButton/index.stories.tsx
+++ src/components/molecules/buttons/UpDownButton/index.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { withKnobs, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 import UpDownButton from '.';
@@ -7,25 +6,11 @@ import UpDownButton from '.';
 export default {
   title: 'molecules/buttons/UpDownButton',
   component: UpDownButton,
-  decorators: [withKnobs],
 } as ComponentMeta<typeof UpDownButton>;
 
 export const Default: ComponentStory<typeof UpDownButton> = ({
-  propNumber,
-  max,
-  min,
   onChange,
+  ...props
 }) => {
-  const propNumberKnob = number('propNumber', 0);
-  const maxKnob = number('max', 10);
-  const minKnob = number('min', 0);
-
-  return (
-    <UpDownButton
-      propNumber={propNumber || propNumberKnob}
-      max={max || maxKnob}
-      min={min || minKnob}
-      onChange={onChange || action('onChnage')}
-    />
-  );
+  return <UpDownButton onChange={onChange || action('onChnage')} {...props} />;
 };
