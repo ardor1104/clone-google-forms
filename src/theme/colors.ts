afe72247e81a255a25bf11f86db1df589c8f428b
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-04-19 17:05:02 +0900

    color: mainTopMenu color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -6,10 +6,20 @@ export const light = {
       bgColor: palette.white,
     },
   },
+  menu: {
+    mainTopMenu: {
+      bgColor: palette.white,
+    },
+  },
 };
 export const dark = {
   headers: {
     bodyHeader: {
+      bgColor: '#292a2d',
+    },
+  },
+  menu: {
+    mainTopMenu: {
       bgColor: '#292a2d',
     },
   },
