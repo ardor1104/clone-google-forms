a6d1cf29564160d55c436da922a2a57ad46be966
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-04-28 11:06:05 +0900

    feat: ✨ add dummy data
    cause there was no backend

--- src/redux/forms/index.type.ts
+++ src/redux/forms/index.type.ts
@@ -2,4 +2,11 @@ export type StateType = {
   viewType: 'listView' | 'goBoardView';
   filter: 'all' | 'own' | 'notOwned';
   sort: 'lastOpened' | 'lastEdit' | 'lastModifiedDate' | 'ascending';
+  list: Array<{
+    id: string;
+    title: string;
+    opened_at: string;
+    updated_at: string;
+    thumbnail: string;
+  }>;
 };
--- src/redux/forms/reducer.ts
+++ src/redux/forms/reducer.ts
@@ -11,6 +11,32 @@ const initialState: StateType = {
   viewType: 'listView',
   filter: 'all',
   sort: 'lastOpened',
+  list: [
+    {
+      id: 'e228d963-4b06-43ca-b1c1-d9f80baf0baa',
+      title: '제목 없는 설문지3',
+      opened_at: '2023-04-03T14:15:08.380319+09:00',
+      updated_at: '2023-04-03T14:15:09.276999+09:00',
+      thumbnail:
+        'https://papers.co/wallpaper/papers.co-so75-blur-gradation-pink-purple-pastel-6-wallpaper.jpg',
+    },
+    {
+      id: '1db832b6-a4a2-4d29-b99b-b2ecf9ed9284',
+      title: '제목 없는 설문지2',
+      opened_at: '2023-04-02T07:43:01.996670+09:00',
+      updated_at: '2023-04-02T07:32:15.697013+09:00',
+      thumbnail:
+        'https://images.unsplash.com/photo-1629196914168-3a2652305f9f?ixlib=rb-4.0.3',
+    },
+    {
+      id: '295c21ab-ab22-4a29-a314-9f8e12d06008',
+      title: '제목 없는 설문지1',
+      opened_at: '2023-04-01T14:15:08.537493+09:00',
+      updated_at: '2023-04-01T14:15:08.091927+09:00',
+      thumbnail:
+        'https://i.pinimg.com/originals/d0/b7/2c/d0b72c0f7022546b01e13d545f0e66fc.jpg',
+    },
+  ],
 };
 
 const formsReducer = (state = initialState, action: ReduxActionsType) => {
