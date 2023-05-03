67ca2de5e35d1ee3235b789a91840cac961c913a
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-05-03 13:43:38 +0900

    feat: formItem type can be changed
    list view
    or
    go board view

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,51 +1,28 @@
 import { useSelector } from 'react-redux';
-import { formsListSelector } from 'redux/forms/selectors';
+import { formsViewTypeSelector } from 'redux/forms/selectors';
 
 import styled from 'styled-components';
 
 import MainTopMenu from 'components/organisms/menu/MainTopMenu';
-import FormBoxItem from 'components/molecules/items/FormBoxItem';
+import ListFormWrapper from 'components/organisms/wrapper/ListFormWrapper';
+import GoBoardFormWrapper from 'components/organisms/wrapper/GoBoardFormWrapper';
 
 const Root = styled.div`
   display: inline-flex;
   flex-direction: column;
 `;
 
-const FormWrapper = styled.div`
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 20px;
-  width: 1150px;
-
-  @media screen and (max-width: ${({ theme }) => theme.breakpoints.xl}) {
-    width: 920px;
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media screen and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    width: 690px;
-    grid-template-columns: repeat(3, 1fr);
-  }
-`;
-
 export default function MainPage(): JSX.Element {
-  const formsList = useSelector(formsListSelector);
+  const formsViewType = useSelector(formsViewTypeSelector);
 
   return (
     <Root>
       <MainTopMenu />
-      <FormWrapper>
-        {formsList.map((formItem) => (
-          <FormBoxItem
-            key={formItem.id}
-            id={formItem.id}
-            title={formItem.title}
-            date={formItem.opened_at}
-            thumbnail={formItem.thumbnail}
-          />
-        ))}
-      </FormWrapper>
+      {formsViewType === 'listView' ? (
+        <ListFormWrapper />
+      ) : (
+        <GoBoardFormWrapper />
+      )}
     </Root>
   );
 }
