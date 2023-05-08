05d04710f909cc15016b34b4d6bd37767e897daa
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-05-08 16:10:23 +0900

    feat: EditFormsTitle modal

---
+++ src/components/organisms/modals/EditFormsTitle/index.tsx
@@ -0,0 +1,133 @@
+import { useState, useEffect, ChangeEvent } from 'react';
+
+import useModal from 'hooks/useModal';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { patchFormsAction, setFormsListItemAction } from 'redux/forms/actions';
+import { modalsPropsSelector } from 'redux/modals/selectors';
+
+import styled from 'styled-components';
+
+import { Modal } from '@transverse/evo-ui';
+
+import Button from 'components/atoms/Button';
+
+const Title = styled.h3`
+  margin-bottom: 16px;
+  color: #202124;
+  font-size: 1.375rem;
+  font-weight: 400;
+`;
+
+const Description = styled.p`
+  color: #5f6368;
+  font-size: ${(props) => props.theme.sizes.font.lg};
+  font-weight: 400;
+`;
+
+const InputWrapper = styled.div`
+  margin: 24px 0;
+  width: 100%;
+`;
+
+const FormsTitleInput = styled.input`
+  border: 1px solid #9aa0a6;
+  border-top: 1px solid #c0c0c0;
+  border-radius: 4px;
+  padding: 1px 8px;
+  width: 100%;
+  height: 25px;
+  font-size: ${(props) => props.theme.sizes.font.md};
+  outline: none;
+
+  &:hover {
+    border: 1px solid #5f6368;
+  }
+
+  &:focus {
+    border: 1px solid #673ab7;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  column-gap: 16px;
+`;
+
+export default function EditFromsTitle(): JSX.Element {
+  const dispatch = useDispatch();
+
+  const modalProps = useSelector(modalsPropsSelector('editFormsTitle'));
+
+  const [formsTitle, setFormsTitle] = useState<string>('');
+
+  const { closeModal } = useModal();
+
+  const doCloseModal = (): void => {
+    closeModal('editFormsTitle');
+  };
+
+  const doEditTitle = (): void => {
+    const id = modalProps?.formsId;
+    const title = formsTitle;
+
+    if (id) {
+      dispatch(
+        patchFormsAction({
+          id,
+          title,
+          succeedFunc: (data) => {
+            dispatch(setFormsListItemAction({ item: data }));
+            doCloseModal();
+          },
+        }),
+      );
+    }
+  };
+
+  const onFormsTitleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = e.currentTarget;
+
+    setFormsTitle(value);
+  };
+
+  useEffect(() => {
+    if (modalProps?.formsTitle) {
+      setFormsTitle(modalProps?.formsTitle);
+    }
+  }, [modalProps?.formsTitle]);
+
+  return (
+    <Modal
+      overrides={{
+        Root: {
+          css: `
+            border-radius: 8px;
+            padding: 30px 42px;
+            width: 100%;
+            max-width: 430px;
+          `,
+        },
+      }}
+      isOpen
+      onClose={doCloseModal}
+    >
+      <Title>이름 바꾸기</Title>
+      <Description>항목의 새 이름을 입력하세요.</Description>
+      <InputWrapper>
+        <FormsTitleInput value={formsTitle} onChange={onFormsTitleChange} />
+      </InputWrapper>
+      <ButtonWrapper>
+        <Button kind='mono' onClick={doCloseModal}>
+          취소
+        </Button>
+        <Button kind='primary' onClick={doEditTitle}>
+          확인
+        </Button>
+      </ButtonWrapper>
+    </Modal>
+  );
+}
