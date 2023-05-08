7cd7b1b0d98e8f56234c1ad6f3d793397ce226ce
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-05-08 16:11:03 +0900

    fix: use FormsViewOutputSerializer type

--- src/redux/forms/index.type.ts
+++ src/redux/forms/index.type.ts
@@ -1,14 +1,9 @@
+import { FormsViewOutputSerializer } from 'api/serializer.type';
+
 export type StateType = {
   viewType: 'listView' | 'goBoardView';
   filter: 'all' | 'own' | 'notOwned';
   sort: 'lastOpened' | 'lastEdit' | 'lastModifiedDate' | 'ascending';
   listViewTitle: null | 'today' | 'week' | 'before';
-  list: Array<{
-    id: string;
-    title: string;
-    opened_at: string;
-    updated_at: string;
-    thumbnail: string;
-    ownerName: string;
-  }>;
+  list: Array<FormsViewOutputSerializer>;
 };
