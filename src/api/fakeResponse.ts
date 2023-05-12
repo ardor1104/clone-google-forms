6395670656af211a4b612b588bf11371dc05dfb3
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-05-12 14:18:32 +0900

    fix: fake response data

--- src/api/fakeResponse.ts
+++ src/api/fakeResponse.ts
@@ -16,23 +16,23 @@ export function fakeGetForms({
     if (sort === 'lastOpened') {
       return [
         {
-          id: 'bdd1e380-ad04-42ae-84e5-3fbcbd74bf39',
-          title: '제목 없는 설문지5',
-          opened_at: '2023-05-03T12:00:00.000000+09:00',
-          updated_at: '2023-05-04T12:00:00.000000+09:00',
-          thumbnail: 'https://wallpaper.dog/large/20538421.jpg',
+          id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
+          title: '제목 없는 설문지4',
+          opened_at: '2023-05-06T12:00:00.000000+09:00',
+          updated_at: '2023-04-29T12:00:00.000000+09:00',
+          thumbnail:
+            'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
           owner: {
             id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
             name: 'TEAZLE',
           },
         },
         {
-          id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
-          title: '제목 없는 설문지4',
-          opened_at: '2023-04-28T12:00:00.000000+09:00',
-          updated_at: '2023-04-29T12:00:00.000000+09:00',
-          thumbnail:
-            'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
+          id: 'bdd1e380-ad04-42ae-84e5-3fbcbd74bf39',
+          title: '제목 없는 설문지5',
+          opened_at: '2023-05-03T12:00:00.000000+09:00',
+          updated_at: '2023-05-04T12:00:00.000000+09:00',
+          thumbnail: 'https://wallpaper.dog/large/20538421.jpg',
           owner: {
             id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
             name: 'TEAZLE',
@@ -68,7 +68,7 @@ export function fakeGetForms({
         {
           id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
           title: '제목 없는 설문지4',
-          opened_at: '2023-04-28T12:00:00.000000+09:00',
+          opened_at: '2023-05-06T12:00:00.000000+09:00',
           updated_at: '2023-04-29T12:00:00.000000+09:00',
           thumbnail:
             'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
@@ -105,7 +105,7 @@ export function fakeGetForms({
         {
           id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
           title: '제목 없는 설문지4',
-          opened_at: '2023-04-28T12:00:00.000000+09:00',
+          opened_at: '2023-05-06T12:00:00.000000+09:00',
           updated_at: '2023-04-29T12:00:00.000000+09:00',
           thumbnail:
             'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
@@ -261,7 +261,7 @@ export function fakeGetForms({
       {
         id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
         title: '제목 없는 설문지4',
-        opened_at: '2023-04-28T12:00:00.000000+09:00',
+        opened_at: '2023-05-06T12:00:00.000000+09:00',
         updated_at: '2023-04-29T12:00:00.000000+09:00',
         thumbnail:
           'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
@@ -273,6 +273,130 @@ export function fakeGetForms({
     ];
   } else if (keyword === '5') {
     return [
+      {
+        id: 'bdd1e380-ad04-42ae-84e5-3fbcbd74bf39',
+        title: '제목 없는 설문지5',
+        opened_at: '2023-05-03T12:00:00.000000+09:00',
+        updated_at: '2023-05-04T12:00:00.000000+09:00',
+        thumbnail: 'https://wallpaper.dog/large/20538421.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
+          name: 'TEAZLE',
+        },
+      },
+    ];
+  } else if (sort === 'lastOpened') {
+    return [
+      {
+        id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
+        title: '제목 없는 설문지4',
+        opened_at: '2023-05-06T12:00:00.000000+09:00',
+        updated_at: '2023-04-29T12:00:00.000000+09:00',
+        thumbnail:
+          'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
+          name: 'TEAZLE',
+        },
+      },
+      {
+        id: 'bdd1e380-ad04-42ae-84e5-3fbcbd74bf39',
+        title: '제목 없는 설문지5',
+        opened_at: '2023-05-03T12:00:00.000000+09:00',
+        updated_at: '2023-05-04T12:00:00.000000+09:00',
+        thumbnail: 'https://wallpaper.dog/large/20538421.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
+          name: 'TEAZLE',
+        },
+      },
+      {
+        id: 'e228d963-4b06-43ca-b1c1-d9f80baf0baa',
+        title: '제목 없는 설문지3',
+        opened_at: '2023-04-03T14:15:08.380319+09:00',
+        updated_at: '2023-04-04T14:15:09.276999+09:00',
+        thumbnail:
+          'https://papers.co/wallpaper/papers.co-so75-blur-gradation-pink-purple-pastel-6-wallpaper.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
+          name: 'TEAZLE',
+        },
+      },
+      {
+        id: '1db832b6-a4a2-4d29-b99b-b2ecf9ed9284',
+        title: '제목 없는 설문지2',
+        opened_at: '2023-04-02T07:43:01.996670+09:00',
+        updated_at: '2023-04-02T08:32:15.697013+09:00',
+        thumbnail:
+          'https://images.unsplash.com/photo-1629196914168-3a2652305f9f?ixlib=rb-4.0.3',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca0',
+          name: 'Alive',
+        },
+      },
+      {
+        id: '295c21ab-ab22-4a29-a314-9f8e12d06008',
+        title: '제목 없는 설문지1',
+        opened_at: '2023-04-01T14:15:08.091927+09:00',
+        updated_at: '2023-04-01T14:15:08.537493+09:00',
+        thumbnail:
+          'https://i.pinimg.com/originals/d0/b7/2c/d0b72c0f7022546b01e13d545f0e66fc.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca1',
+          name: 'Delmont',
+        },
+      },
+    ];
+  } else if (sort === 'ascending') {
+    return [
+      {
+        id: '295c21ab-ab22-4a29-a314-9f8e12d06008',
+        title: '제목 없는 설문지1',
+        opened_at: '2023-04-01T14:15:08.091927+09:00',
+        updated_at: '2023-04-01T14:15:08.537493+09:00',
+        thumbnail:
+          'https://i.pinimg.com/originals/d0/b7/2c/d0b72c0f7022546b01e13d545f0e66fc.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca1',
+          name: 'Delmont',
+        },
+      },
+      {
+        id: '1db832b6-a4a2-4d29-b99b-b2ecf9ed9284',
+        title: '제목 없는 설문지2',
+        opened_at: '2023-04-02T07:43:01.996670+09:00',
+        updated_at: '2023-04-02T08:32:15.697013+09:00',
+        thumbnail:
+          'https://images.unsplash.com/photo-1629196914168-3a2652305f9f?ixlib=rb-4.0.3',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca0',
+          name: 'Alive',
+        },
+      },
+      {
+        id: 'e228d963-4b06-43ca-b1c1-d9f80baf0baa',
+        title: '제목 없는 설문지3',
+        opened_at: '2023-04-03T14:15:08.380319+09:00',
+        updated_at: '2023-04-04T14:15:09.276999+09:00',
+        thumbnail:
+          'https://papers.co/wallpaper/papers.co-so75-blur-gradation-pink-purple-pastel-6-wallpaper.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
+          name: 'TEAZLE',
+        },
+      },
+      {
+        id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
+        title: '제목 없는 설문지4',
+        opened_at: '2023-05-06T12:00:00.000000+09:00',
+        updated_at: '2023-04-29T12:00:00.000000+09:00',
+        thumbnail:
+          'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
+        owner: {
+          id: '3b01d37f-bffb-40bf-87ee-6f00cf348ca6',
+          name: 'TEAZLE',
+        },
+      },
       {
         id: 'bdd1e380-ad04-42ae-84e5-3fbcbd74bf39',
         title: '제목 없는 설문지5',
@@ -301,7 +425,7 @@ export function fakeGetForms({
       {
         id: 'edd42b41-89d8-4aae-bb9f-ff7fe8bade07',
         title: '제목 없는 설문지4',
-        opened_at: '2023-04-28T12:00:00.000000+09:00',
+        opened_at: '2023-05-06T12:00:00.000000+09:00',
         updated_at: '2023-04-29T12:00:00.000000+09:00',
         thumbnail:
           'https://thumbs.dreamstime.com/b/flowers-composition-pattern-made-chamomiles-petals-pastel-purple-background-spring-summer-concept-flat-lay-top-view-copy-144695810.jpg',
