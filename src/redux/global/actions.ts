2a5099db1c50303ff84a73897da5ae38f69d9534
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-05-16 10:17:05 +0900

    fix: remove example code

--- src/redux/global/actions.ts
+++ src/redux/global/actions.ts
@@ -1,18 +1,8 @@
 import {
-  GET_EXAMPLE_SAGA,
   SET_GLOBAL_LEFT_MENU_IS_VISIBLE,
   TOGGLE_GLOBAL_LEFT_MENU_IS_VISIBLE,
   SET_GLOBAL_MAIN_PAGE_LIST_VIEW_TITLE,
 } from 'redux/constants';
-
-export const getExampleAction = (payload: {
-  exampleId: string;
-  onSucceed?: (data: any) => void;
-  onFail?: () => void;
-}) => ({
-  type: GET_EXAMPLE_SAGA,
-  payload,
-});
 
 export const setGlobalLeftMenuIsVisibleAction = (payload: {
   isVisible: any;
--- src/store/saga.ts
+++ src/store/saga.ts
@@ -1,7 +1,5 @@
-import globalSaga from 'redux/global/sagas';
 import formsSaga from 'redux/forms/sagas';
 
 export default {
-  global: globalSaga,
   forms: formsSaga,
 };
